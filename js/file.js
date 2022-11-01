21e14c668c819137a693ca700c4b133d8fc27181
LuigginaRossi <luigginavaldivia@gmail.com>
2022-11-01 01:09:02 +0100

    created ticket price calculation

---
+++ js/file.js
@@ -0,0 +1,47 @@
+//chiedo informazioni all'utente
+
+const nameUser = prompt("Inserisci il nome completo del passeggero");
+
+const ageUser = parseInt(prompt("Inserisci età del passeggero"));
+
+const km = parseInt(prompt("Inserisci il numero di km che devi percorrere"));
+
+//trovo elemento nell'html
+
+const divPassangerInfo = document.getElementById("passenger-info");
+const divPriceTicket = document.getElementById("ticket-price");
+
+
+//calcolo prezzo biglietto:
+
+const priceTicket = km * .21;
+
+console.log(priceTicket);
+
+//sconto 20% per minorenni
+
+const discount20 = priceTicket * .8;
+
+console.log(discount20)
+
+//sconto 40% over 65
+const discount40 = priceTicket * .6;
+
+console.log(discount40)
+
+//modifico valori nell'html
+
+divPassangerInfo.innerHTML = `Passeggero: ${nameUser}. <br> Età: ${ageUser} anni. <br> Percorrenza: ${km}Km.`
+
+console.log(divPassangerInfo);
+
+//condizioni:
+
+if (ageUser <= 18) {
+    divPriceTicket.innerHTML = `Il prezzo del tuo biglietto è: ${discount20}`;
+    console.log(div)
+} else if (ageUser >= 65) {
+    divPriceTicket.innerHTML = `Il prezzo del tuo biglietto è: ${discount40}`;
+} else {
+    divPriceTicket.innerHTML = `Il prezzo del tuo biglietto è: ${priceTicket}`;
+}
